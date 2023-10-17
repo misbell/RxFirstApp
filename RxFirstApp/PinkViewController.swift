f3e4bb4d4d554362d23253fe3fa305cde1f61e30
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-10-17 17:26:32 -0400

    new

---
+++ RxFirstApp/PinkViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PinkViewController.swift
+//  RxFirstApp
+//
+//  Created by Michael Prenez-Isbell on 10/17/23.
+//
+
+import UIKit
+
+class PinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "pink screen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
